ddd272dc2e90cd1d8bd1a31de3bad14fd0f66771
2gayatri <121083796+2gayatri@users.noreply.github.com>
2022-12-20 12:51:37 +0530

    first commit

--- fact.c
+++ fact.c
@@ -1,12 +1,20 @@
 #include(stdio.h)
 void fact();
-{
+
 main()
+{
   fact();
   }
 void fact()
 {
-  
+int n,i,f=1;
+  printf("\n enter no=");
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
+  {
+    f=i+1;
+  }
+  printf("\n factorial=%d",f)
   
   
   
